36cdb7edc3a92c962d2f29558037f7be998278b1
David Sanders <david.sanders@mail.utoronto.ca>
2017-03-03 03:25:54 -0500

    recurrent_id format change; now specify directly which layer it mimicks using repeat_layer=x

--- architecture/TF_Model.py
+++ architecture/TF_Model.py
@@ -15,9 +15,7 @@ class TF_Model:
         self.filter_widths = []
         self.filter_strides = []
         self.num_filters = []
-        self.recurrent_ids = []
-        self.recurrent_layer = [] # Indicates which layer weights to use
-        self.recurrent_layer_set = []
+        self.layer_reference = []
 
         # Read config file
         self.parse_config()
@@ -29,7 +27,7 @@ class TF_Model:
         f = open(self.model_dir + '/config.txt', 'r')
 
         current_line = 1
-        prev_layer_units = 0
+        is_first_line = True
         for line in f:
             # Remove newline
             line = line.rstrip('\n')
@@ -45,10 +43,12 @@ class TF_Model:
 
             # If not empty line, parse info
             if(len(line) > 0):
-                prev_layer_units = self.parse_line(current_line, line, prev_layer_units)
-                current_line = current_line + 1
+                self.parse_line(current_line, line, is_first_line)
+                is_first_line = False
+        
+            current_line = current_line + 1
 
-    def parse_line(self, current_line, line, prev_layer_units):
+    def parse_line(self, current_line, line, is_first_line):
 
         # Layer parameter initialization
         num_neurons = 0
@@ -57,7 +57,7 @@ class TF_Model:
         filter_width = 0
         filter_stride = 0
         num_filters = 0
-        recurrent_id = 0
+        repeated_layer = 0
 
         # Error string
         err_str = 'Error in configuration file on line ' + str(current_line)
@@ -82,15 +82,15 @@ class TF_Model:
                     num_filters = int(temp[2])
                 else:
                     sys.exit(err_str + ": invalid connectivity syntax")
-            elif(param_name == 'recurrent_id'):
-                recurrent_id = int(param_value)
+            elif(param_name == 'repeat_layer'):
+                repeated_layer = int(param_value)
 
         # Assign parameters
-        if (num_neurons == 0 and (filter_width==0 or filter_stride==0 or num_filters==0) ):
+        if (num_neurons == 0 and (filter_width==0 or filter_stride==0 or num_filters==0)  and repeated_layer==0):
             sys.exit(err_str + ": invalid layer connection specification")
-        elif (activation_function == '' and current_line > 1):
+        elif (activation_function == '' and not is_first_line):
             sys.exit(err_str + ": activation function must be specified")
-        elif (activation_function != '' and current_line == 1):
+        elif (activation_function != '' and is_first_line):
             sys.exit(err_str + ": activation function must not be specified for the input layer")
 
         self.num_units.append(num_neurons)
@@ -99,41 +99,49 @@ class TF_Model:
         self.filter_widths.append(filter_width)
         self.filter_strides.append(filter_stride)
         self.num_filters.append(num_filters)
-        self.recurrent_ids.append(recurrent_id)
+        self.layer_reference.append(repeated_layer)
 
     def create_model(self):
         self.weights = []
+        weights_exist = []
+
         num_layers = len(self.num_units)
+        
+        # Update IO sizes
+        self.layer_reference[0] = 1
+        for connections in range(num_layers-1):
+            current_connection = connections+1
+            input_layer = current_connection - 1
+            output_layer = current_connection
 
-        for i in range(num_layers-1):
-            current_layer = i+1
+            weight_exist = True
+            layer_reference = self.layer_reference[output_layer]
 
-            weights_exist = True
-            input_num = self.num_units[current_layer - 1]
-            output_num = self.num_units[current_layer]
-            filter_width = self.filter_widths[current_layer]
-            filter_stride = self.filter_strides[current_layer]
-            num_filters = self.num_filters[current_layer]
-            partially_connected = self.partially_connected[current_layer]
-            recurrent_id = self.recurrent_ids[current_layer]
-
-            if(recurrent_id == 0):
-                self.recurrent_layer.append(current_layer)
+            if(layer_reference == 0):
+                self.layer_reference[output_layer] = output_layer+1
             else:
-                found = False
-                for id_set in self.recurrent_layer_set:
-                    if(id_set[0] == recurrent_id):
-                        found = True
-                        self.recurrent_layer.append(id_set[1])
-                        weights_exist = False
-                        break
-                if(not found):
-                    self.recurrent_layer_set.append((recurrent_id, current_layer))
-                    self.recurrent_layer.append(current_layer)
+                self.num_units[output_layer] = self.num_units[layer_reference-1]
+                weight_exist = False
+
+            weights_exist.append(weight_exist)
+
+        # Create weights
+        for connections in range(num_layers-1):
+            current_connection = connections+1
+            input_layer = current_connection - 1
+            output_layer = current_connection
+
+            weight_exist = weights_exist[connections]
+            input_num = self.num_units[input_layer]
+            output_num = self.num_units[output_layer]
+            filter_width = self.filter_widths[current_connection]
+            filter_stride = self.filter_strides[current_connection]
+            num_filters = self.num_filters[current_connection]
+            partially_connected = self.partially_connected[current_connection]
 
             W = 0
             b = 0
-            if(weights_exist):
+            if(weight_exist):
                 output_per_filter = 0
                 if(partially_connected):
                     output_per_filter = (input_num - filter_width) / filter_stride + 1
@@ -143,12 +151,12 @@ class TF_Model:
                     init_weight = math.sqrt(3/(filter_width + output_num))
 
                     W = tf.Variable(tf.random_normal([filter_width, 1, num_filters], mean=0.0, stddev=init_weight))
-                    b = tf.Variable(tf.random_normal([num_filters], mean=0.0, stddev=init_weight))
+                    b = tf.Variable(tf.zeros([num_filters]))
                 else:
                     init_weight = math.sqrt(3/(input_num + output_num))
 
                     W = tf.Variable(tf.random_normal([input_num, output_num], mean=0.0, stddev=init_weight))
-                    b = tf.Variable(tf.random_normal([output_num], mean=0.0, stddev=init_weight))
+                    b = tf.Variable(tf.zeros([output_num]))
 
             self.weights.append((W,b))
 
@@ -165,7 +173,7 @@ class TF_Model:
 
             partially_connected = self.partially_connected[current_layer]
             activation_function = self.activation_functions[current_layer]
-            weights = self.weights[self.recurrent_layer[current_layer-1]-1]
+            weights = self.weights[self.layer_reference[current_layer]-2]
             W = weights[0]
             b = weights[1]
 
