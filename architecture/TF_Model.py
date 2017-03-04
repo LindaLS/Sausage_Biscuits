2bd15569cc2301ba42d8a8fe2dd88226a5358171
David Sanders <david.sanders@mail.utoronto.ca>
2017-03-04 00:28:43 -0500

    add support for inverted_layer

--- architecture/TF_Model.py
+++ architecture/TF_Model.py
@@ -16,6 +16,7 @@ class TF_Model:
         self.filter_strides = []
         self.num_filters = []
         self.layer_reference = []
+        self.weight_inverted = []
 
         # Read config file
         self.parse_config()
@@ -58,6 +59,8 @@ class TF_Model:
         filter_stride = 0
         num_filters = 0
         repeated_layer = 0
+        invert_layer = 0
+        weight_inverted = False
 
         # Error string
         err_str = 'Error in configuration file on line ' + str(current_line)
@@ -84,6 +87,9 @@ class TF_Model:
                     sys.exit(err_str + ": invalid connectivity syntax")
             elif(param_name == 'repeat_layer'):
                 repeated_layer = int(param_value)
+            elif(param_name == 'invert_layer'):
+                repeated_layer = int(param_value)
+                weight_inverted = True
 
         # Assign parameters
         if (num_neurons == 0 and (filter_width==0 or filter_stride==0 or num_filters==0)  and repeated_layer==0):
@@ -100,6 +106,7 @@ class TF_Model:
         self.filter_strides.append(filter_stride)
         self.num_filters.append(num_filters)
         self.layer_reference.append(repeated_layer)
+        self.weight_inverted.append(weight_inverted)
 
     def create_model(self):
         self.weights = []
@@ -111,52 +118,75 @@ class TF_Model:
         self.layer_reference[0] = 1
         for connections in range(num_layers-1):
             current_connection = connections+1
-            input_layer = current_connection - 1
-            output_layer = current_connection
+            input_layer_index = current_connection - 1
+            output_layer_index = current_connection
 
             weight_exist = True
-            layer_reference = self.layer_reference[output_layer]
+            layer_reference = self.layer_reference[output_layer_index]
+            weight_inverted = self.weight_inverted[current_connection]
 
             if(layer_reference == 0):
-                self.layer_reference[output_layer] = output_layer+1
+                self.layer_reference[output_layer_index] = output_layer_index+1
             else:
-                self.num_units[output_layer] = self.num_units[layer_reference-1]
+                self.num_units[output_layer_index] = self.num_units[layer_reference-1]
                 weight_exist = False
+
+                self.partially_connected[output_layer_index] = self.partially_connected[layer_reference-1]
+                self.partially_connected[output_layer_index] = self.filter_widths[layer_reference-1]
+                self.partially_connected[output_layer_index] = self.filter_strides[layer_reference-1]
+                self.partially_connected[output_layer_index] = self.filter_strides[layer_reference-1]
+
+            if(weight_inverted):
+                num_output = self.num_units[layer_reference-1]
+                num_input = self.num_units[layer_reference-2]
+                self.num_units[output_layer_index] = num_input
+                self.num_units[input_layer_index] = num_output
+
+
+            partially_connected = self.partially_connected[current_connection]
+            if(partially_connected):
+                input_num = self.num_units[input_layer_index]
+                output_num = self.num_units[output_layer_index]
+                filter_width = self.filter_widths[current_connection]
+                filter_stride = self.filter_strides[current_connection]
+                num_filters = self.num_filters[current_connection]
+                output_per_filter = (input_num - filter_width) / filter_stride + 1
+                output_num = int(output_per_filter * num_filters)
+                self.num_units[current_connection] = output_num
 
             weights_exist.append(weight_exist)
 
         # Create weights
         for connections in range(num_layers-1):
             current_connection = connections+1
-            input_layer = current_connection - 1
-            output_layer = current_connection
+            input_layer_index = current_connection - 1
+            output_layer_index = current_connection
 
-            weight_exist = weights_exist[connections]
-            input_num = self.num_units[input_layer]
-            output_num = self.num_units[output_layer]
+            weight_exist = weights_exist[input_layer_index]
+            input_num = self.num_units[input_layer_index]
+            output_num = self.num_units[output_layer_index]
             filter_width = self.filter_widths[current_connection]
             filter_stride = self.filter_strides[current_connection]
             num_filters = self.num_filters[current_connection]
             partially_connected = self.partially_connected[current_connection]
+            weight_inverted = self.weight_inverted[current_connection]
 
             W = 0
-            b = 0
+            b = tf.Variable(tf.zeros([output_num]))
             if(weight_exist):
                 output_per_filter = 0
                 if(partially_connected):
                     output_per_filter = (input_num - filter_width) / filter_stride + 1
                     output_num = int(output_per_filter * num_filters)
-                    self.num_units[i+1] = output_num
+                    self.num_units[current_connection] = output_num
 
                     init_weight = math.sqrt(3/(filter_width + output_num))
 
                     W = tf.Variable(tf.random_normal([filter_width, 1, num_filters], mean=0.0, stddev=init_weight))
-                    b = tf.Variable(tf.zeros([num_filters]))
                 else:
                     init_weight = math.sqrt(3/(input_num + output_num))
-
                     W = tf.Variable(tf.random_normal([input_num, output_num], mean=0.0, stddev=init_weight))
-                    b = tf.Variable(tf.zeros([output_num]))
+                    
 
             self.weights.append((W,b))
 
@@ -168,21 +198,32 @@ class TF_Model:
         temp = tf.shape(x)
         num_inputs = temp[0]
         current = x
-        for i in range(output_layer-1):
-            current_layer = i+1
 
-            partially_connected = self.partially_connected[current_layer]
-            activation_function = self.activation_functions[current_layer]
-            weights = self.weights[self.layer_reference[current_layer]-2]
-            W = weights[0]
+        for i in range(output_layer-1):
+            current_connection = i+1
+            input_layer_index = i
+            output_layer_index = i+1
+
+            partially_connected = self.partially_connected[current_connection]
+            activation_function = self.activation_functions[current_connection]
+            weight_inverted = self.weight_inverted[current_connection]
+
+            weights = self.weights[current_connection-1]
             b = weights[1]
+            W = 0
+            if(not weight_inverted):
+                weights = self.weights[self.layer_reference[output_layer_index]-2]
+                W = weights[0]
+            else:
+                weights = self.weights[self.layer_reference[output_layer_index]-2]
+                W = tf.transpose(weights[0])
 
             if (i == len(self.activation_functions)-1):
                 current = tf.nn.dropout(current, dropout)
             if(partially_connected):
                 filter_stride = self.filter_strides[i+1]
                 conv = tf.nn.conv1d(tf.expand_dims(current,-1), W, stride=filter_stride, padding="VALID")
-                z = tf.reshape(conv + b, [num_inputs, -1])
+                z = tf.reshape(conv, [num_inputs, -1]) + b
             else:
                 z = tf.matmul(current, W) + b
 
